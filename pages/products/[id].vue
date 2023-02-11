c4ddb783128af9e2f5c3a71b3feaeb4744740429
何柳杰 <268079@163.com>
2023-02-11 13:44:10 +0800

    feat: get route param by config vue file

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>Product id is {{  id  }}</p>
+  </div>
+</template>
+
+<script setup>
+  const { id } = useRoute().params
+  // acess http://localhost:3000/products/123, you'll see the value
+</script>
+
+<style scoped>
+
+</style>
