0b7d7f840338e70e084330e9eaacfd51f0242ad6
何柳杰 <268079@163.com>
2023-02-11 13:58:05 +0800

    feat: add NuxtLink component

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
     <h1>Home Page</h1>
+    <header>
+      <nav>
+        <NuxtLink to="/">Nuxt Dojo</NuxtLink>
+        <ul >
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/products">Merch</NuxtLink></li>
+        </ul>
+        <a href="/about">normal link -- about page</a>
+      </nav>
+    </header>
   </div>
 </template>
 
