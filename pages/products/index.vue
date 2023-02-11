065ddbfde598013f560d40ca4b12d52f879c621c
何柳杰 <268079@163.com>
2023-02-11 15:02:38 +0800

    feat: use useFetch to fetch data

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
-    <h1>Products Home Page</h1>
+    <div class="grid grid-cols-4 gap-5">
+      <div v-for="p in products" :key="p.id">
+        <NuxtLink :to="`/products/${p.id}`">{{ p.title }}</NuxtLink>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts">
+<script setup>
+  //  fetch the products
+  const { data: products } = await useFetch('https://fakestoreapi.com/products')
+
   definePageMeta({
     layout: "products",
   })
 </script>
-
-<style scoped>
-
-</style>
